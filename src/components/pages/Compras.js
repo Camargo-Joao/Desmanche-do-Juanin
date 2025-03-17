ec752c96cadd925d12fb7c71e7eef187340db82b
Camargo-Joao <camargo.jp06@gmail.com>
2025-03-17 10:55:13 -0300

    validation-form

--- src/components/pages/Compras.js
+++ src/components/pages/Compras.js
@@ -4,29 +4,37 @@ import Button from "../layout/ui/Button";
 
 import "./Compras.css";
 
+function validarform() {
+  input = document.getElementsByClassName("input-content").value
+
+  if(input != null) {
+    window.open()
+  }
+}
+
 function Compras() {
   return (
     <div className="main-content">
       <h1 className="title">Tela de Pagamento</h1>
       <h3 className="subtitle">Só aceitamos cartão de crédito!</h3>
       <form className="formulario">
-        <Input type="text" label="Número do cartão:" />
+        <Input type="text" label="Número do cartão:" togg="togg-yes" />
 
-        <Input type="text" label="Nome Impresso no cartão:" />
+        <Input type="text" label="Nome Impresso no cartão:" togg="togg-yes" />
 
         <Input type="text" label="Validade:" />
 
         <Input type="text" label="CVV:" />
-
+       
         <Input type="text" label="CEP + Informações endereço:" />
 
         <Input type="text" label="Cidade:"/>
 
         <Input type="text" label="Bairro:" />
         
-        <Input type="text" label="Complemento:>" />
+        <Input type="text" label="Complemento:" />
 
-        <Button path="/finalizar-compra">Concluir pedidossssfsdf</Button>
+        <Button click={validarform()}>Concluir pedidos</Button>
       </form>
     </div>
   );
